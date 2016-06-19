5b78dc2cc14fe88dd567d5eea55e2aeba0f8fe4a
thenumber24 <lott.dylan@gmail.com>
2016-06-19 21:47:52 +0000

    working on updates

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 module.exports = {
+	//DATABASE CONFIGURATION 
 	// the database url to connect
-	url : 'mongodb://localhost:27017/mydbdash'
+	// url: 'mongodb://localhost:27017/mydbdash'
+	url: 'mongodb://'+ process.env.DB_USER +':'+ process.env.DB_USER_PASSWORD +'pass@ds025782.mlab.com:25782/hivemindnews'
 	
 }
